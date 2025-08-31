f3cac01f693baaeb6f2f3d4a95fcb0d978b267d4
Berinchris2 <berinchristo1@gmail.com>
2025-08-31 17:49:28 +0530

    Day 1 Done

--- Day1.py
+++ Day1.py
@@ -1,3 +1,12 @@
 
 print("Hello world!\nHello world!\nHello world!")
-print("Hello" + " " + "Berin")
+#print("Hello" + " " + "Berin")
+Name = input("What is your Name? ")
+print(len(Name))
+print("Hello " + Name + "!" + " You are " + input("What is your Age? ") + " years old.")
+
+#Band Name Generator
+print("Welcome to the Band Name Generator.")
+City = input("What is the name of the city you grew up in?\n")
+Pet = input("What is the name of your pet?\n")
+print("Your band name could be " + City + " " + Pet)
